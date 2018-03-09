ac2f778d3888b111e458a00e8a7e91163656719f
= <->
2018-03-09 17:13:53 +0700

    modified main and fix window 1

--- drawingWindow.py
+++ drawingWindow.py
@@ -36,12 +36,28 @@ class Simple_drawing_window(Simple_drawing_window) :
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Window 1")
+        self.cat = QPixmap("images/cat1.jpg")
 
     def paintEvent(self, e):
-        super().paintEvent(self, e)
         p = QPainter()
         p.begin(self)
-        p.drawPixmap(QRect(0,0,100,100), self.rabbit)
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(20,255,0))
+        p.drawPolygon(
+            QPoint(70,100), QPoint(100,110),
+            QPoint(130,100), QPoint(100,150)
+        )
+
+        p.setPen(QColor(100,127,0))
+        p.setBrush(QColor(100,127,0))
+        p.drawPie(50,150,100,100,0,180*16)
+
+        p.drawPolygon(
+            QPoint(50,200), QPoint(150,200), QPoint(100,400),
+        )
+
+        p.drawPixmap(QRect(200,100,336,203),self.cat)
         p.end()
 # end tawan modification
 
@@ -49,8 +65,23 @@ class Simple_drawing_window(Simple_drawing_window) :
 def main():
     app = QApplication(sys.argv)
 
-    w = Simple_drawing_window()
-    w.show()
+    try:
+        w = Simple_drawing_window1()
+        w.show()
+    except:
+        print("no win 1 implementer")
+
+    try:
+        w = Simple_drawing_window2()
+        w.show()
+    except:
+        print("no win 2 implementer")
+
+    try:
+        w = Simple_drawing_window3()
+        w.show()
+    except:
+        print("no win 3 implementer")
 
     return app.exec_()
 
