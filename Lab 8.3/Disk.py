d60057be7673e468902da14a940a5d1b585cd45c
Dan Suwanseree <ayw.dansuwanseree@gmail.com>
2018-03-09 18:06:49 +0700

    Added disk implementation
    for 8.3

--- Lab 8.3/Disk.py
+++ Lab 8.3/Disk.py
@@ -1,15 +1,54 @@
-import turtle
+from turtle import *
 
 class Disk(object):
-    def __init__(self):
-        pass
+    def __init__(self, dname, xpos, ypos, dh, dw, col):
+        self.name = dname
+        self.x = xpos
+        self.y = ypos
+        self.h = dh
+        self.w = dw
+        self.color = col
 
     def showdisk(self):
-        pass
+        Turtle.pu()
+        Turtle.goto(x,y)
+        Turtle.pd()
+        Turtle.fillcolor(color)
+        Turtle.begin_fill()
+        Turtle.seth(0)
+        Turtle.fd(w / 2)
+        Turtle.lt(90)
+        Turtle.fd(h)
+        Turtle.lt(90)
+        Turtle.fd(w)
+        Turtle.lt(90)
+        Turtle.fd(h)
+        Turtle.lt(90)
+        Turtle.fd(w / 2)
+        Turtle.end_fill()
 
     def newpos(self,x,y):
-        pass
+        self.x = x
+        self.y = y
 
     def cleardisk(self):
-        pass
+        Turtle.pu()
+        Turtle.goto(x,y)
+        Turtle.pd()
+        Turtle.fillcolor("white")
+        Turtle.pencolor("white")
+        Turtle.begin_fill()
+        Turtle.seth(0)
+        Turtle.fd(w / 2)
+        Turtle.lt(90)
+        Turtle.fd(h)
+        Turtle.lt(90)
+        Turtle.fd(w)
+        Turtle.lt(90)
+        Turtle.fd(h)
+        Turtle.lt(90)
+        Turtle.fd(w / 2)
+        Turtle.end_fill()
+        Turtle.pencolor("black")
+        Turtle.fillcolor(color)
 
