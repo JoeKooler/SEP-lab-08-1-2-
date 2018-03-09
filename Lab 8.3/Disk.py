fd71a572363bc333256270cc1056d9e2dbbb4f21
Dan Suwanseree <ayw.dansuwanseree@gmail.com>
2018-03-09 18:16:26 +0700

    Fixed disk

--- Lab 8.3/Disk.py
+++ Lab 8.3/Disk.py
@@ -10,45 +10,59 @@ class Disk(object):
         self.color = col
 
     def showdisk(self):
-        Turtle.pu()
-        Turtle.goto(x,y)
-        Turtle.pd()
-        Turtle.fillcolor(color)
-        Turtle.begin_fill()
-        Turtle.seth(0)
-        Turtle.fd(w / 2)
-        Turtle.lt(90)
-        Turtle.fd(h)
-        Turtle.lt(90)
-        Turtle.fd(w)
-        Turtle.lt(90)
-        Turtle.fd(h)
-        Turtle.lt(90)
-        Turtle.fd(w / 2)
-        Turtle.end_fill()
+        pu()
+        goto(x,y)
+        pd()
+        fillcolor(color)
+        begin_fill()
+        seth(0)
+        fd(w / 2)
+        lt(90)
+        fd(h)
+        lt(90)
+        fd(w)
+        lt(90)
+        fd(h)
+        lt(90)
+        fd(w / 2)
+        end_fill()
 
     def newpos(self,x,y):
         self.x = x
         self.y = y
 
     def cleardisk(self):
-        Turtle.pu()
-        Turtle.goto(x,y)
-        Turtle.pd()
-        Turtle.fillcolor("white")
-        Turtle.pencolor("white")
-        Turtle.begin_fill()
-        Turtle.seth(0)
-        Turtle.fd(w / 2)
-        Turtle.lt(90)
-        Turtle.fd(h)
-        Turtle.lt(90)
-        Turtle.fd(w)
-        Turtle.lt(90)
-        Turtle.fd(h)
-        Turtle.lt(90)
-        Turtle.fd(w / 2)
-        Turtle.end_fill()
-        Turtle.pencolor("black")
-        Turtle.fillcolor(color)
+        pu()
+        goto(x,y)
+        pd()
+        fillcolor("white")
+        pencolor("white")
+        begin_fill()
+        seth(0)
+        fd(w / 2)
+        lt(90)
+        fd(h)
+        lt(90)
+        fd(w)
+        lt(90)
+        fd(h)
+        lt(90)
+        fd(w / 2)
+        end_fill()
+        pencolor("black")
+        fillcolor(color)
 
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
+    def getname(self):
+        return self.name
+
+    def geth(self):
+        return self.h
+
+    def getw(self):
+        return self.w
