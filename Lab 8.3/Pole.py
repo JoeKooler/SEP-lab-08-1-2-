e658a76a0df760611086a863030ac584a7eab372
= <->
2018-03-09 17:48:46 +0700

    init 8.3

---
+++ Lab 8.3/Pole.py
@@ -0,0 +1,15 @@
+import turtle
+import Pole
+
+class Pole(object):
+    def __init__(self):
+        pass
+
+    def showpole(self):
+        pass
+
+    def pushdisk(self):
+        pass
+
+    def popdisk(self):
+        pass
